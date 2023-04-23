c35ceac17ff9a85cd662fdb22f251b3bebd4b45d
nikhilt <nikhil.t1226@gmail.com>
2023-04-23 11:57:33 +0100

    jenkins

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -6,13 +6,15 @@ def call() {
 
             stage('Init') {
                 steps  {
+//                    sh 'terraform init -backend-config=env-dev/state.tfvars'
                     sh 'terraform init -backend-config=env-dev/state.tfvars'
                 }
             }
 
             stage('Apply') {
                 steps {
-                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+//                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    sh 'terraform destroy -auto-approve -var-file=env-dev/main.tfvars'
                 }
             }
         }
