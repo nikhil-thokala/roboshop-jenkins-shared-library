a5ef1707989f7194ba4802ec2bd1b3ca14436382
nikhilt <nikhil.t1226@gmail.com>
2023-04-23 18:56:40 +0100

    jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -16,5 +16,5 @@ def testcases() {
 }
 
 def codequality(){
-    sh 'sonar-scanner' -Dsonar.host.url=//172.31.2.104:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectkey=${component}
+    sh 'sonar-scanner' -Dsonar.host.url=//172.31.2.104:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component}
 }
