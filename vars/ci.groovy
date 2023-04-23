735bfb9c0ee1fb5a88b1bf182ef4256d79633096
nikhilt <nikhil.t1226@gmail.com>
2023-04-23 17:23:19 +0100

    jenkins

---
+++ vars/ci.groovy
@@ -0,0 +1,26 @@
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Compile/Build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
+                }
+            }
+
+
+            stage('Test Cases') {
+                steps {
+                    script {
+                        common.testcases()
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ vars/common.groovy
@@ -0,0 +1,17 @@
+def compile() {
+    if(app_lang == "nodejs") {
+        sh 'npm install'
+    }
+    if(app_lang == "maven") {
+        sh 'mvn package'
+    }
+}
+
+def testcases() {
+    if(app_lang == "nodejs") {
+        sh 'echo test'
+    }
+    if(app_lang == "maven") {
+        sh 'echo test'
+    }
+}
